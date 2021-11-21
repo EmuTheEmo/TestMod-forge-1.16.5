09ae2970b893123bb14ec6f9b35a8a544be74c9b
EmuTheEmo <jaysgames.jsg@gmail.com>
2021-11-21 19:51:05 +0000

    Added armor

--- src/main/java/com/jay/testingmod/item/ModItems.java
+++ src/main/java/com/jay/testingmod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.jay.testingmod.item;
 
 import com.jay.testingmod.TestingMod;
 import com.jay.testingmod.item.custom.Firestone;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -44,6 +45,26 @@ public class ModItems {
     //Hoe
     public static final RegistryObject<Item> AMETHYST_HOE = ITEMS.register("amethyst_hoe",
             () -> new HoeItem(ModItemTier.AMETHYST, 0, 0f,
+                    new Item.Properties().group(ModItemGroup.TESTING_GROUP)));
+
+    //Helmet
+    public static final RegistryObject<Item> AMETHYST_HELMET = ITEMS.register("amethyst_helmet",
+            () -> new ArmorItem(ModArmorMaterial.AMETHYST, EquipmentSlotType.HEAD,
+                    new Item.Properties().group(ModItemGroup.TESTING_GROUP)));
+
+    //Chestplate
+    public static final RegistryObject<Item> AMETHYST_CHESTPLATE = ITEMS.register("amethyst_chestplate",
+            () -> new ArmorItem(ModArmorMaterial.AMETHYST, EquipmentSlotType.CHEST,
+                    new Item.Properties().group(ModItemGroup.TESTING_GROUP)));
+
+    //Leggings
+    public static final RegistryObject<Item> AMETHYST_LEGGINGS = ITEMS.register("amethyst_leggings",
+            () -> new ArmorItem(ModArmorMaterial.AMETHYST, EquipmentSlotType.LEGS,
+                    new Item.Properties().group(ModItemGroup.TESTING_GROUP)));
+
+    //Boots
+    public static final RegistryObject<Item> AMETHYST_BOOT = ITEMS.register("amethyst_boots",
+            () -> new ArmorItem(ModArmorMaterial.AMETHYST, EquipmentSlotType.FEET,
                     new Item.Properties().group(ModItemGroup.TESTING_GROUP)));
 
     public static void register(IEventBus eventBus) {
