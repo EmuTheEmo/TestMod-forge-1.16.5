02aa3d4120480c70de12fb434a82af595b798322
EmuTheEmo <jaysgames.jsg@gmail.com>
2021-11-26 16:02:59 +0000

    Custom tree generation

---
+++ src/main/java/com/jay/testingmod/world/ModWorldEvents.java
@@ -0,0 +1,16 @@
+package com.jay.testingmod.world;
+
+import com.jay.testingmod.TestingMod;
+import com.jay.testingmod.world.gen.ModTreeGeneration;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TestingMod.MOD_ID)
+public class ModWorldEvents {
+
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+        ModTreeGeneration.generateTrees(event);
+    }
+}
